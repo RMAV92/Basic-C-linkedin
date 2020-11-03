81212bf249a94ac05d88d4fa691dbd210ab38c42
RMAV <ramon.abeijonvidal@gmail.com>
2020-11-03 17:48:37 +0100

    Modificaciones del 02/11/2020

--- infoEmpleado.c
+++ infoEmpleado.c
@@ -1,6 +1,36 @@
+#include <stdio.h>
+int anioActual= 2020;
+#define PRECIO_HORAS_EXTRA 60;
 int main(){
 
-  int cantidadDeBytes 0 sizeof(int);
+  int edad;
+  int anioNacimiento;
+  int horasExtraTrabajadas;
+  int haRealizadoHorasExtra;
+  
+  float sueldoAnual;
+  float totalHorasExtraAPagar;
+  char departamento;
 
-    return 0;
+
+  printf("\n\n A continuación vamos a pedirle algunos datos personales:\n\n");
+  printf("¿Que edad tiene?:");
+  scanf("%d", &edad);
+
+  printf("¿Cual es su sueldo anual?:");
+  scanf("%f", &sueldoAnual);
+
+  printf("¿A que departamento pertenece?:");
+  scanf(" %c", &departamento);
+
+  printf("¿Cuantas horas extra has trabajado este año?: ");
+  scanf("%d", &horasExtraTrabajadas);
+
+  anioNacimiento = anioActual - edad;
+  totalHorasExtraAPagar = horasExtraTrabajadas * PRECIO_HORAS_EXTRA;
+  haRealizadoHorasExtra = totalHorasExtraAPagar;
+
+
+  return 0;
+
 }
